285b171558f64bf02f9a01686a591a0ce2d75a35
carlo-ddm <carloduminuco90@gmail.com>
2022-05-30 14:20:11 +0200

    impostazione sezione tre parte top

--- src/components/MainComp.vue
+++ src/components/MainComp.vue
@@ -2,14 +2,16 @@
   <main>
     <SecOne />
     <SecTwo />
+    <SecThree />
   </main>
 </template>
 
 <script>
 import SecOne from "./sections/SecOne.vue";
 import SecTwo from "./sections/SecTwo.vue";
+import SecThree from "./sections/SecThree.vue";
 export default {
-    components: { SecOne, SecTwo }
+    components: { SecOne, SecTwo, SecThree }
 }
 </script>
 
---
+++ src/components/sections/SecThree.vue
@@ -0,0 +1,39 @@
+<template>
+  <section>
+    <div class="top d-flex align-items-center">
+      <div class="container">
+        <div class="row">
+          <div class="col-6 cd1">
+            <h1>ciao</h1>
+          </div>
+          <div class="col-6 cd2">
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam adipisci ducimus animi pariatur ab, accusantium error commodi hic ut laboriosam facere dicta, assumenda quis rem ea dolores accusamus esse! Laudantium.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="bottom">
+      bottom
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'SecThree'
+}
+</script>
+
+<style lang="scss" scoped>
+.col-6 {
+  border: 1px solid black
+}
+
+section {
+  width: 100%;
+  height: 800px;
+  .top {
+    height: calc(30%);
+  }
+}
+</style>

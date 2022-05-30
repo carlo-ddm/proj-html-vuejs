46a53c72de4b75b30ed03fc5c268b01880151bb7
carlo-ddm <carloduminuco90@gmail.com>
2022-05-30 16:47:42 +0200

    sezion 3! correzioni simone

---
+++ src/components/section-cards/SectionCardsTwo.vue
@@ -0,0 +1,26 @@
+<template>
+<div class="cd_card">
+  <img :src="`${card.card}`" alt="">
+</div>
+</template>
+
+<script>
+export default {
+  name: 'SectionCardsTwo',
+  props: {
+    card: Object
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.cd_card {
+  flex-basis: 24.5%;
+  height: 24.5%;
+  margin-bottom: 10px;
+  img {
+    width: 100%;
+  }
+}
+
+</style>

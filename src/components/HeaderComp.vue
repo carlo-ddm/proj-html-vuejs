043195c9c4ac1ffe958354d2995fe70be3c5538e
carlo-ddm <carloduminuco90@gmail.com>
2022-05-31 15:02:51 +0200

    correzioni header top

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -1,27 +1,30 @@
 <template>
   <header>
-<!-- TOP -->
-    <div class="cd-container-top d-flex align-items-center">
-      <div class="cdd_container d-flex justify-content-between">
-        <div>
+    <!-- TOP -->
+    <div class="top">
+      <div class="cdd_container d-flex justify-content-between align-items-center">
+
+        <div class="tel-contacts">
           <i class="fa-solid fa-phone"></i>
-          <span>
-            (001)88451234 88455438
-          </span>
+          <span>(001)88451234</span>
+          <span>88455438</span>
         </div>
-        <!-- social in HTML? -->
+
+        <!-- social -->
         <div>
           <i class="fa-brands fa-facebook-f"></i>
           <i class="fa-brands fa-twitter"></i>
           <i class="fa-brands fa-pinterest-p"></i>
         </div>
+
       </div>
     </div>
 
-<!-- BOTTOM -->
+    <!-- BOTTOM -->
     <div class="cd-container-bottom d-flex align-items-center">
-      <div class="cdd_container d-flex justify-content-between align-items-center">
-
+      <div
+        class="cdd_container d-flex justify-content-between align-items-center"
+      >
         <div>
           LOGO
           <span>Phlox</span>
@@ -31,7 +34,7 @@
         <nav>
           <ul class="m-0 p-0">
             <li v-for="(link, index) in nav" :key="`_link_${index}`">
-              <a :href="link.href"> {{link.pointer}} </a>
+              <a :href="link.href"> {{ link.pointer }} </a>
             </li>
           </ul>
         </nav>
@@ -40,77 +43,83 @@
           <div class="cd-btn">Purchase</div>
           <i class="fa-solid fa-magnifying-glass"></i>
         </div>
-
       </div>
     </div>
-    </header>
+  </header>
 </template>
 
 <script>
 export default {
-  name: 'HeaderComp',
+  name: "HeaderComp",
 
   data() {
     return {
-
       // Nav
       nav: [
         {
-          pointer: 'home',
-          href: '#'
+          pointer: "home",
+          href: "#",
         },
         {
-          pointer: 'about',
-          href: '#'
+          pointer: "about",
+          href: "#",
         },
         {
-          pointer: 'service',
-          href: '#'
+          pointer: "service",
+          href: "#",
         },
         {
-          pointer: 'showcase',
-          href: '#'
+          pointer: "showcase",
+          href: "#",
         },
         {
-          pointer: 'contact',
-          href: '#'
+          pointer: "contact",
+          href: "#",
         },
-      ]
-    }
-  }
-}
+      ],
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
 // Import VARS
-@import '../assets/styles/vars';
+@import "../assets/styles/vars";
 // Import MIXINES
-@import '../assets/styles/mixines';
+@import "../assets/styles/mixines";
 // Import UTILITIES
-@import '../assets/styles/utilities.scss';
+@import "../assets/styles/utilities.scss";
 
 header {
-  
-
   // TOP
-  .cd-container-top {
-    width: 100%;
+  .top {
     height: 35px;
-    background-color: #3D3D3D;
-    color: white;
+    background-color: #3d3d3d;
 
-    span {
-      margin-left: 2px;
-      font-size: $primary-font;
-    }
-    
-    div:nth-last-child(1) {
-      i {
-        margin: 0 5px;
+    .cdd_container {
+      height: 35px;
+      background-color: #3d3d3d;
+      color: white;
+
+      .tel-contacts {
+        i {
+          margin-right: 5px;
+        }
+        span {
+          margin-right: 5px;
+          font-size: $primary-font;
+        }
+  
+      }
+
+      div:nth-last-child(1) {
+        i {
+          margin: 0 5px;
+        }
       }
     }
   }
-  
+
   // BOTTOM
   .cd-container-bottom {
     width: 100%;
@@ -121,7 +130,7 @@ header {
       ul {
         @include list-style;
         li a {
-          @include a-style
+          @include a-style;
         }
       }
     }
@@ -131,5 +140,4 @@ header {
     }
   }
 }
-
 </style>
